443398427b68ba3c7dd07c1522838a95f1e1dc83
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2022-01-04 02:53:17 -0500

    limpiando caracteres extraños

--- popup_crear_lead.php
+++ popup_crear_lead.php
@@ -11,7 +11,7 @@ if (isset($_POST['swError']) && ($_POST['swError'] != "")) { //Para saber si ha 
 
 if (isset($_POST['P']) && ($_POST['P'] != "")) {
     try {
-		
+
         //Carpeta de archivos anexos
         $i = 0; //Archivos
         $RutaAttachSAP = ObtenerDirAttach();
@@ -33,35 +33,35 @@ if (isset($_POST['P']) && ($_POST['P'] != "")) {
         closedir($route);
         $CantFiles = count($DocFiles);
 
-		$Anexos=array();
+        $Anexos = array();
 
-		//Mover los anexos a la carpeta de archivos de SAP
-		$j = 0;
-		while ($j < $CantFiles) {
-			$Archivo = FormatoNombreAnexo($DocFiles[$j]);
-			$NuevoNombre = $Archivo[0];
-			$OnlyName = $Archivo[1];
-			$Ext = $Archivo[2];
+        //Mover los anexos a la carpeta de archivos de SAP
+        $j = 0;
+        while ($j < $CantFiles) {
+            $Archivo = FormatoNombreAnexo($DocFiles[$j]);
+            $NuevoNombre = $Archivo[0];
+            $OnlyName = $Archivo[1];
+            $Ext = $Archivo[2];
 
-			if (file_exists($dir_new)) {
-				copy($dir . $DocFiles[$j], $dir_new . $NuevoNombre);
-				copy($dir_new . $NuevoNombre, $RutaAttachSAP[0] . $NuevoNombre);
+            if (file_exists($dir_new)) {
+                copy($dir . $DocFiles[$j], $dir_new . $NuevoNombre);
+                copy($dir_new . $NuevoNombre, $RutaAttachSAP[0] . $NuevoNombre);
 
-				//Anexos
-				array_push($Anexos,array(
-					"id_anexo" => $j,
-					"tipo_documento" => 2,//ObjType de Socios de negocios
-					"id_documento" => null,
-					"archivo" => $OnlyName,
-					"ext_archivo" => $Ext,
-					"metodo" => 1,
-					"fecha" => FormatoFechaToSAP(date('Y-m-d')),
-					"id_usuario" => intval($_SESSION['CodUser']),
-					"comentarios" => ""
-				));
-			}
-			$j++;
-		}
+                //Anexos
+                array_push($Anexos, array(
+                    "id_anexo" => $j,
+                    "tipo_documento" => 2, //ObjType de Socios de negocios
+                    "id_documento" => null,
+                    "archivo" => $OnlyName,
+                    "ext_archivo" => $Ext,
+                    "metodo" => 1,
+                    "fecha" => FormatoFechaToSAP(date('Y-m-d')),
+                    "id_usuario" => intval($_SESSION['CodUser']),
+                    "comentarios" => "",
+                ));
+            }
+            $j++;
+        }
 
         //Dividir el nombre en Nombre y Segundo nombre
         $Nombres = explode(" ", $_POST['PNNombres']);
@@ -180,7 +180,7 @@ if (isset($_POST['P']) && ($_POST['P'] != "")) {
                     "metodo" => 1,
                 ),
             ),
-			"documentos_Anexos" => $Anexos,
+            "documentos_Anexos" => $Anexos,
             "metodo" => 1,
             "prop1" => (isset($_POST['Prop1']) && ($_POST['Prop1'] == "Y")) ? $_POST['Prop1'] : "N",
             "prop2" => (isset($_POST['Prop2']) && ($_POST['Prop2'] == "Y")) ? $_POST['Prop2'] : "N",
@@ -194,19 +194,19 @@ if (isset($_POST['P']) && ($_POST['P'] != "")) {
             "crear_proyecto" => (isset($_POST['CrearProyecto']) && ($_POST['CrearProyecto'] == "Y")) ? $_POST['CrearProyecto'] : "N",
         );
 
-        // Para probar
-        $Cabecera_json = json_encode($Cabecera);
-        echo $Cabecera_json;
+        // Para probar, en caso contrario comentar
+        //$Cabecera_json = json_encode($Cabecera);
+        //echo $Cabecera_json;
         // Comentar esto
-        /*$Metodo = "SociosNegocios/Asistente";
-    $Resultado = EnviarWebServiceSAP($Metodo, $Cabecera, true, true);
-
-    if ($Resultado->Success == 0) {
-    $sw_error = 1;
-    $msg_error = $Resultado->Mensaje;
-    } else {
-    header('Location:popup_crear_lead.php?a=' . base64_encode("OK_SNAdd"));
-    }*/
+        $Metodo = "SociosNegocios/Asistente";
+        $Resultado = EnviarWebServiceSAP($Metodo, $Cabecera, true, true);
+        if ($Resultado->Success == 0) {
+            $sw_error = 1;
+            $msg_error = $Resultado->Mensaje;
+        } else {
+            header('Location:popup_crear_lead.php?a=' . base64_encode("OK_SNAdd"));
+        }
+		// Hasta aquí
     } catch (Exception $e) {
         echo 'Excepcion capturada: ', $e->getMessage(), "\n";
     }
@@ -528,7 +528,7 @@ while ($row_MedioPago = sqlsrv_fetch_array($SQL_MedioPago)) {?>
 											</thead>
 											<tbody>
 											<?php
-												while ($row = sqlsrv_fetch_array($SQL_Prop)) {?>
+while ($row = sqlsrv_fetch_array($SQL_Prop)) {?>
 													<tr>
 														<td><?php echo $row['CodigoGrupo']; ?></td>
 														<td><?php echo $row['NombreGrupo']; ?></td>
@@ -550,7 +550,7 @@ while ($row_MedioPago = sqlsrv_fetch_array($SQL_MedioPago)) {?>
 								<div class="panel-body">
 									<div class="row">
 										<form action="upload.php" class="dropzone" id="dropzoneForm" name="dropzoneForm">
-											<?php if($sw_error==0){LimpiarDirTemp();}?>
+											<?php if ($sw_error == 0) {LimpiarDirTemp();}?>
 											<div class="fallback">
 												<input name="File" id="File" type="file" form="dropzoneForm" />
 											</div>
@@ -745,13 +745,13 @@ function CrearProyecto(){
 <script>
  Dropzone.options.dropzoneForm = {
 		paramName: "File", // The name that will be used to transfer the file
-		maxFilesize: "<?php echo ObtenerVariable("MaxSizeFile");?>", // MB
-	 	maxFiles: "<?php echo ObtenerVariable("CantidadArchivos");?>",
+		maxFilesize: "<?php echo ObtenerVariable("MaxSizeFile"); ?>", // MB
+	 	maxFiles: "<?php echo ObtenerVariable("CantidadArchivos"); ?>",
 		uploadMultiple: true,
 		addRemoveLinks: true,
 		dictRemoveFile: "Quitar",
-	 	acceptedFiles: "<?php echo ObtenerVariable("TiposArchivos");?>",
-		dictDefaultMessage: "<strong>Haga clic aqui para cargar anexos</strong><br>Tambien puede arrastrarlos hasta aqui<br><h4><small>(máximo <?php echo ObtenerVariable("CantidadArchivos");?> archivos a la vez)<small></h4>",
+	 	acceptedFiles: "<?php echo ObtenerVariable("TiposArchivos"); ?>",
+		dictDefaultMessage: "<strong>Haga clic aqui para cargar anexos</strong><br>Tambien puede arrastrarlos hasta aqui<br><h4><small>(máximo <?php echo ObtenerVariable("CantidadArchivos"); ?> archivos a la vez)<small></h4>",
 		dictFallbackMessage: "Tu navegador no soporta cargue de archivos mediante arrastrar y soltar",
 	 	removedfile: function(file) {
 		  $.get( "includes/procedimientos.php", {
