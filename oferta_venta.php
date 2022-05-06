88a77b978d2640c4d9fa364378e9c4b084bba0be
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2022-05-06 09:14:34 -0500

    pasando la oferta a producción presipitadamente

--- oferta_venta.php
+++ oferta_venta.php
@@ -217,23 +217,17 @@ if (isset($_GET['dt_LS']) && ($_GET['dt_LS']) == 1) { //Verificar que viene de u
     //Contacto cliente
     $SQL_ContactoCliente = Seleccionar('uvw_Sap_tbl_ClienteContactos', '*', "CodigoCliente='" . base64_decode($_GET['Cardcode']) . "'", 'NombreContacto');
 
-    //Sucursal cliente
-    $SQL_SucursalDestino = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente=''" . base64_decode($_GET['Cardcode']) . "''", 'NombreSucursal');
-
-    //Orden de servicio
+	//Sucursales, SMM 06/05/2022
+	$SQL_SucursalDestino = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . base64_decode($_GET['Cardcode']) . "' AND TipoDireccion='S'", 'NombreSucursal');
+	$SQL_SucursalFacturacion = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . base64_decode($_GET['Cardcode']) . "' AND TipoDireccion='B'", 'NombreSucursal');
+    
+	//Orden de servicio
     $SQL_OrdenServicioCliente = Seleccionar('uvw_Sap_tbl_LlamadasServicios', '*', "ID_LlamadaServicio='" . base64_decode($_GET['LS']) . "'");
 }
 
 // SMM, 26/04/2022
 if (isset($_GET['dt_OF']) && ($_GET['dt_OF']) == 1) { //Verificar que viene de una Oferta de ventas (Duplicar)
     $dt_OF = 1;
-
-    //Clientes
-    $SQL_Cliente = Seleccionar('uvw_Sap_tbl_Clientes', '*', "CodigoCliente='" . base64_decode($_GET['Cardcode']) . "'", 'NombreCliente');
-    $row_Cliente = sqlsrv_fetch_array($SQL_Cliente);
-
-    //Contacto cliente
-    //$SQL_ContactoCliente=Seleccionar('uvw_Sap_tbl_ClienteContactos','*',"CodigoCliente='".base64_decode($_GET['Cardcode'])."'",'NombreContacto');
 
     $ParametrosCopiarOfertaToOrden = array(
         "'" . base64_decode($_GET['OF']) . "'",
@@ -254,6 +248,17 @@ if (isset($_GET['dt_OF']) && ($_GET['dt_OF']) == 1) { //Verificar que viene de u
 		});
 		</script>";
     }
+
+	//Clientes
+	$SQL_Cliente = Seleccionar('uvw_Sap_tbl_Clientes', '*', "CodigoCliente='" . base64_decode($_GET['Cardcode']) . "'", 'NombreCliente');
+	$row_Cliente = sqlsrv_fetch_array($SQL_Cliente);
+
+	//Contacto cliente
+	$SQL_ContactoCliente = Seleccionar('uvw_Sap_tbl_ClienteContactos', '*', "CodigoCliente='" . base64_decode($_GET['Cardcode']) . "'", 'NombreContacto');
+
+	//Sucursales, SMM 06/05/2022
+	$SQL_SucursalDestino = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . base64_decode($_GET['Cardcode']) . "' AND TipoDireccion='S'", 'NombreSucursal');
+	$SQL_SucursalFacturacion = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . base64_decode($_GET['Cardcode']) . "' AND TipoDireccion='B'", 'NombreSucursal');
 }
 
 if ($edit == 1 && $sw_error == 0) {
@@ -276,10 +281,9 @@ if ($edit == 1 && $sw_error == 0) {
     //Clientes
     $SQL_Cliente = Seleccionar('uvw_Sap_tbl_Clientes', '*', "CodigoCliente='" . $row['CardCode'] . "'", 'NombreCliente');
 
-    //Sucursales
-    $SQL_SucursalFacturacion = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . $row['CardCode'] . "' and TipoDireccion='B'", 'NombreSucursal');
-
-    $SQL_SucursalDestino = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . $row['CardCode'] . "' and TipoDireccion='S'", 'NombreSucursal');
+    //Sucursales, SMM 06/05/2022
+    $SQL_SucursalDestino = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . $row['CardCode'] . "' AND TipoDireccion='S'", 'NombreSucursal');
+    $SQL_SucursalFacturacion = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . $row['CardCode'] . "' AND TipoDireccion='B'", 'NombreSucursal');
 
     //Contacto cliente
     $SQL_ContactoCliente = Seleccionar('uvw_Sap_tbl_ClienteContactos', '*', "CodigoCliente='" . $row['CardCode'] . "'", 'NombreContacto');
@@ -308,10 +312,9 @@ if ($sw_error == 1) {
     //Clientes
     $SQL_Cliente = Seleccionar('uvw_Sap_tbl_Clientes', '*', "CodigoCliente='" . $row['CardCode'] . "'", 'NombreCliente');
 
-    //Sucursales
-    $SQL_SucursalFacturacion = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . $row['CardCode'] . "' and TipoDireccion='B'", 'NombreSucursal');
-
-    $SQL_SucursalDestino = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . $row['CardCode'] . "' and TipoDireccion='S'", 'NombreSucursal');
+    //Sucursales, SMM 06/05/2022
+    $SQL_SucursalDestino = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . $row['CardCode'] . "' AND TipoDireccion='S'", 'NombreSucursal');
+    $SQL_SucursalFacturacion = Seleccionar('uvw_Sap_tbl_Clientes_Sucursales', '*', "CodigoCliente='" . $row['CardCode'] . "' AND TipoDireccion='B'", 'NombreSucursal');
 
     //Contacto cliente
     $SQL_ContactoCliente = Seleccionar('uvw_Sap_tbl_ClienteContactos', '*', "CodigoCliente='" . $row['CardCode'] . "'", 'NombreContacto');
@@ -571,46 +574,45 @@ function CrearLead(){
 					$('.ibox-content').toggleClass('sk-loading', false);
 				}
 			});
-			<?php }?>
 
-			<?php if ($edit == 0 && $sw_error == 0 && $dt_OF == 0) { // Para que no recargue las sucursales en la edición. ?>
-				$.ajax({
-					type: "POST",
-					url: "ajx_cbo_select.php?type=3&tdir=S&id="+carcode,
-					success: function(response){
-						$('#SucursalDestino').html(response).fadeIn();
-						$('#SucursalDestino').trigger('change');
-					},
-					error: function(error) {
-						console.error(error.responseText);
-						$('.ibox-content').toggleClass('sk-loading', false);
-					}
-				});
-				$.ajax({
-					type: "POST",
-					url: "ajx_cbo_select.php?type=3&tdir=B&id="+carcode,
-					success: function(response){
-						$('#SucursalFacturacion').html(response).fadeIn();
-						$('#SucursalFacturacion').trigger('change');
-					},
-					error: function(error) {
-						console.error(error.responseText);
-						$('.ibox-content').toggleClass('sk-loading', false);
-					}
-				});
+			// Recargar sucursales.
+			$.ajax({
+				type: "POST",
+				url: "ajx_cbo_select.php?type=3&tdir=S&id="+carcode,
+				success: function(response){
+					$('#SucursalDestino').html(response).fadeIn();
+					$('#SucursalDestino').trigger('change');
+				},
+				error: function(error) {
+					console.error(error.responseText);
+					$('.ibox-content').toggleClass('sk-loading', false);
+				}
+			});
+			$.ajax({
+				type: "POST",
+				url: "ajx_cbo_select.php?type=3&tdir=B&id="+carcode,
+				success: function(response){
+					$('#SucursalFacturacion').html(response).fadeIn();
+					$('#SucursalFacturacion').trigger('change');
+				},
+				error: function(error) {
+					console.error(error.responseText);
+					$('.ibox-content').toggleClass('sk-loading', false);
+				}
+			});
 
-				// Recargar condición de pago. Dependiendo del cliente en la creación, 28/04/2022
-				$.ajax({
-					type: "POST",
-					url: "ajx_cbo_select.php?type=7&id="+carcode,
-					success: function(response){
-						$('#CondicionPago').html(response).fadeIn();
-					},
-					error: function(error) {
-						console.error(error.responseText);
-						$('.ibox-content').toggleClass('sk-loading', false);
-					}
-				});
+			// Recargar condición de pago.
+			$.ajax({
+				type: "POST",
+				url: "ajx_cbo_select.php?type=7&id="+carcode,
+				success: function(response){
+					$('#CondicionPago').html(response).fadeIn();
+				},
+				error: function(error) {
+					console.error(error.responseText);
+					$('.ibox-content').toggleClass('sk-loading', false);
+				}
+			});
 			<?php }?>
 
 			<?php if ($edit == 0) {?>
@@ -1160,20 +1162,26 @@ if ($edit == 1 || $sw_error == 1) {
 					<div class="form-group">
 						<label class="col-lg-1 control-label">Sucursal destino <span class="text-danger">*</span></label>
 						<div class="col-lg-5">
-							<select name="SucursalDestino" class="form-control select2" id="SucursalDestino" required <?php if (($edit == 1) && ($row['Cod_Estado'] == 'C')) {echo "disabled='disabled'";}?>>
-							  <?php if (($edit == 0) && ($dt_LS == 0) && ($dt_OF == 0)) {?><option value="">Seleccione...</option><?php }?>
-							  <?php if (($edit == 1) || ($dt_LS == 1) || ($sw_error == 1)) {while ($row_SucursalDestino = sqlsrv_fetch_array($SQL_SucursalDestino)) {?>
+							<select name="SucursalDestino" class="form-control select2" id="SucursalDestino" required="required" <?php if (($edit == 1) && ($row['Cod_Estado'] == 'C')) {echo "disabled='disabled'";}?>>
+							  <?php if ($edit == 0) {?><option value="">Seleccione...</option><?php }?>
+							  <?php if ($edit == 1 || $sw_error == 1 || $dt_LS == 1 || $dt_OF == 1) {?>
+								  <optgroup label='Dirección de destino'></optgroup>
+								  <?php while ($row_SucursalDestino = sqlsrv_fetch_array($SQL_SucursalDestino)) {?>
 									<option value="<?php echo $row_SucursalDestino['NombreSucursal']; ?>" <?php if ((isset($row['SucursalDestino'])) && (strcmp($row_SucursalDestino['NombreSucursal'], $row['SucursalDestino']) == 0)) {echo "selected=\"selected\"";} elseif (isset($_GET['Sucursal']) && (strcmp($row_SucursalDestino['NombreSucursal'], base64_decode($_GET['Sucursal'])) == 0)) {echo "selected=\"selected\"";}?>><?php echo $row_SucursalDestino['NombreSucursal']; ?></option>
-							  <?php }}?>
+							  	<?php }?>
+							  <?php }?>
 							</select>
 						</div>
 						<label class="col-lg-1 control-label">Sucursal facturación</label>
 						<div class="col-lg-5">
 							<select name="SucursalFacturacion" class="form-control select2" id="SucursalFacturacion" <?php if (($edit == 1) && ($row['Cod_Estado'] == 'C')) {echo "disabled='disabled'";}?>>
-							  <option value="">Seleccione...</option>
-							  <?php if ($edit == 1 || $sw_error == 1) {while ($row_SucursalFacturacion = sqlsrv_fetch_array($SQL_SucursalFacturacion)) {?>
-									<option value="<?php echo $row_SucursalFacturacion['NombreSucursal']; ?>" <?php if ((isset($row['SucursalFacturacion'])) && (strcmp($row_SucursalFacturacion['NombreSucursal'], $row['SucursalFacturacion']) == 0)) {echo "selected=\"selected\"";}?>><?php echo $row_SucursalFacturacion['NombreSucursal']; ?></option>
-							  <?php }}?>
+							<?php if ($edit == 0) {?><option value="">Seleccione...</option><?php }?>
+							  <?php if ($edit == 1 || $sw_error == 1 || $dt_LS == 1 || $dt_OF == 1) {?>
+								  <optgroup label='Dirección de facturas'></optgroup>
+								  <?php while ($row_SucursalFacturacion = sqlsrv_fetch_array($SQL_SucursalFacturacion)) {?>
+									<option value="<?php echo $row_SucursalFacturacion['NombreSucursal']; ?>" <?php if ((isset($row['SucursalFacturacion'])) && (strcmp($row_SucursalFacturacion['NombreSucursal'], $row['SucursalFacturacion']) == 0)) {echo "selected=\"selected\"";} elseif (isset($_GET['SucursalFact']) && (strcmp($row_SucursalFacturacion['NombreSucursal'], base64_decode($_GET['SucursalFact'])) == 0)) {echo "selected=\"selected\"";}?>><?php echo $row_SucursalFacturacion['NombreSucursal']; ?></option>
+							      <?php }?>
+							  <?php }?>
 							</select>
 						</div>
 					</div>
