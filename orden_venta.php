8e8c5ef8cd21881b5abb88fc1d066ad36ee1c8ab
Stiven Muñoz Murillo <smunoz@nedugatech.com>
2023-09-29 17:08:22 -0500

    error coma

--- orden_venta.php
+++ orden_venta.php
@@ -123,7 +123,7 @@ if (isset($_POST['P']) && ($_POST['P'] != "")) { //Grabar Orden de venta
 			"'" . ($_POST['IdMotivoAutorizacion'] ?? "") . "'",
 			"'" . ($_POST['ComentariosAutor'] ?? "") . "'",
 			"'" . ($_POST['MensajeProceso'] ?? "") . "'",
-			"'" . ($_POST['Aprobados'] ?? "0") . "'",, // BasadoOfertaAprobados
+			"'" . ($_POST['Aprobados'] ?? "0") . "'", // BasadoOfertaAprobados
 		);
 
 		$SQL_CabeceraOrdenVenta = EjecutarSP('sp_tbl_OrdenVenta', $ParametrosCabOrdenVenta, $_POST['P']);
