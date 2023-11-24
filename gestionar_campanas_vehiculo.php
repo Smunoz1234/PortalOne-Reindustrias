cc5b520fabec7f27edd10e42526fa1f84ccfb291
Stiven Muñoz Murillo <smunoz@nedugatech.com>
2023-11-24 10:22:07 -0500

    filtro de marcas

--- gestionar_campanas_vehiculo.php
+++ gestionar_campanas_vehiculo.php
@@ -3,6 +3,9 @@
 PermitirAcceso(337);
 $sw = 0;
 
+// SMM, 23/11/2023
+$SQL_Marca = Seleccionar('uvw_Sap_tbl_TarjetasEquipos_MarcaVehiculo', '*');
+
 // Fechas
 $FI_FechaVigencia = "";
 $FF_FechaVigencia = "";
@@ -14,29 +17,38 @@ if (isset($_GET['FI_FechaVigencia']) && $_GET['FI_FechaVigencia'] != "") {
 	$fecha = date('Y-m-d');
 	$nuevafecha = strtotime('-' . ObtenerVariable("DiasRangoFechasGestionar") . ' day');
 	$nuevafecha = date('Y-m-d', $nuevafecha);
-	$FI_FechaVigencia = $nuevafecha;
+	// $FI_FechaVigencia = $nuevafecha;
 }
 if (isset($_GET['FF_FechaVigencia']) && $_GET['FF_FechaVigencia'] != "") {
 	$FF_FechaVigencia = $_GET['FF_FechaVigencia'];
 	$sw = 1;
 } else {
-	$FF_FechaVigencia = date('Y-m-d');
+	// $FF_FechaVigencia = date('Y-m-d');
 }
 
 // Filtros
 $Estado = $_GET['Estado'] ?? "";
+$BuscarDato = $_GET['BuscarDato'] ?? "";
+
+$IdMarca = $_GET['Marca'] ?? "";
 $Campana = $_GET['Campana'] ?? "";
 $Proveedor = $_GET['Proveedor'] ?? "";
 $Sucursal = $_GET['Sucursal'] ?? "";
-$BuscarDato = $_GET['BuscarDato'] ?? "";
+
+// SMM, 23/11/2023
+if($Estado != "") {
+	$sw = 1;
+	$Filtro = "estado = '$Estado'";
+}
 
 if ($sw == 1) {
-	$Filtro = "(fecha_limite_vigencia BETWEEN '" . FormatoFecha($FI_FechaVigencia) . "' AND '" . FormatoFecha($FF_FechaVigencia) . "')";
-
-	$Filtro .= ($Estado == "") ? "" : "AND estado = '$Estado'";
+	$Filtro .= ($FI_FechaVigencia == "") ? "" : "AND fecha_limite_vigencia >= '" . FormatoFecha($FI_FechaVigencia) . "'";
+	$Filtro .= ($FF_FechaVigencia == "") ? "" : "AND fecha_limite_vigencia >= '" . FormatoFecha($FF_FechaVigencia) . "'";
+	
 	$Filtro .= ($Campana == "") ? "" : "AND id_campana = '$Campana'";
+	$Filtro .= ($IdMarca == "") ? "" : "AND id_marca = '$IdMarca'";
 	$Filtro .= ($Proveedor == "") ? "" : "AND id_socio_negocio = '$Proveedor'";
-	$Filtro .= ($Sucursal == "") ? "" : "AND id_consecutivo_direccion = '$Sucursal'";
+	$Filtro .= ($Sucursal == "") ? "" : "AND id_direccion_destino = '$Sucursal'";
 
 	$Filtro .= ($BuscarDato == "") ? "" : " AND (
 		id_campana LIKE '%$BuscarDato%' OR
@@ -45,8 +57,9 @@ if ($sw == 1) {
 		direccion_destino LIKE '%$BuscarDato%'
 	)";
 
+	// SMM, 23/11/2023
 	$Cons = "SELECT * FROM tbl_CampanaVehiculos WHERE $Filtro";
-	// $Cons = "SELECT * FROM tbl_CampanaVehiculos";
+	
 	// echo $Cons;
 	$SQL = sqlsrv_query($conexion, $Cons);
 
@@ -152,21 +165,6 @@ if ($sw == 1) {
 								</div>
 
 								<div class="form-group">
-									<label class="col-lg-1 control-label">Fechas Vigentes</label>
-									<div class="col-lg-3">
-										<div class="input-daterange input-group">
-											<input name="FI_FechaVigencia" type="text"
-												class="input-sm form-control fecha" id="FI_FechaVigencia"
-												placeholder="Fecha inicial" value="<?php echo $FI_FechaVigencia; ?>"
-												autocomplete="off" />
-											<span class="input-group-addon">hasta</span>
-											<input name="FF_FechaVigencia" type="text"
-												class="input-sm form-control fecha" id="FF_FechaVigencia"
-												placeholder="Fecha final" value="<?php echo $FF_FechaVigencia; ?>"
-												autocomplete="off" />
-										</div>
-									</div>
-
 									<label class="col-lg-1 control-label">
 										Estado Campaña <span class="text-danger">*</span>
 									</label>
@@ -187,9 +185,42 @@ if ($sw == 1) {
 										<input name="Campana" type="text" class="form-control" id="Campana"
 											maxlength="100" value="<?php echo $Campana; ?>">
 									</div>
+
+									<!-- Inicio, #Marca -->
+									<label class="col-lg-1 control-label">Marca</label>
+									
+									<div class="col-lg-3">
+										<select name="Marca" id="Marca" class="form-control select2">
+											<option value="">Seleccione...</option>
+											
+											<?php while ($row_Marca = sqlsrv_fetch_array($SQL_Marca)) { ?>
+												<option value="<?php echo $row_Marca['IdMarcaVehiculo']; ?>" <?php if ((isset($_GET['Marca'])) && ($row_Marca['IdMarcaVehiculo'] == $_GET['Marca'])) {
+														echo "selected";
+												   	} ?>>
+													<?php echo $row_Marca['DeMarcaVehiculo']; ?>
+												</option>
+											<?php } ?>
+										</select>
+									</div>
+									<!-- Fin, #Marca -->
 								</div>
 
 								<div class="form-group">
+									<label class="col-lg-1 control-label">Fechas Vigentes</label>
+									<div class="col-lg-3">
+										<div class="input-daterange input-group">
+											<input name="FI_FechaVigencia" type="text"
+												class="input-sm form-control fecha" id="FI_FechaVigencia"
+												placeholder="Fecha inicial" value="<?php echo $FI_FechaVigencia; ?>"
+												autocomplete="off" />
+											<span class="input-group-addon">hasta</span>
+											<input name="FF_FechaVigencia" type="text"
+												class="input-sm form-control fecha" id="FF_FechaVigencia"
+												placeholder="Fecha final" value="<?php echo $FF_FechaVigencia; ?>"
+												autocomplete="off" />
+										</div>
+									</div>
+
 									<label class="col-lg-1 control-label">Proveedor</label>
 									<div class="col-lg-3">
 										<input name="Proveedor" type="hidden" id="Proveedor" value="<?php if (isset($_GET['Proveedor']) && ($_GET['Proveedor'] != "")) {
@@ -203,7 +234,7 @@ if ($sw == 1) {
 
 									<label class="col-lg-1 control-label">Sucursal Proveedor</label>
 									<div class="col-lg-3">
-										<select id="Sucursal" name="Sucursal" class="form-control select2">
+										<select id="Sucursal" name="Sucursal" class="form-control">
 											<option value="">(Todos)</option>
 
 											<?php if ($Proveedor != "") { ?>
@@ -217,16 +248,16 @@ if ($sw == 1) {
 											<?php } ?>
 										</select>
 									</div>
+								</div>
 
+								<div class="form-group">
 									<label class="col-lg-1 control-label">Buscar Dato</label>
 									<div class="col-lg-3">
 										<input name="BuscarDato" type="text" class="form-control" id="BuscarDato"
 											maxlength="100" value="<?php echo $BuscarDato; ?>">
 									</div>
-								</div>
 
-								<div class="form-group">
-									<div class="col-lg-12">
+									<div class="col-lg-8">
 										<button type="submit" class="btn btn-outline btn-success pull-right"><i
 												class="fa fa-search"></i> Buscar</button>
 									</div>
@@ -354,6 +385,9 @@ if ($sw == 1) {
 			});
 
 			$('.chosen-select').chosen({ width: "100%" });
+
+			// SMM, 23/11/2023
+			$(".select2").select2();
 
 			var options = {
 				url: function (phrase) {
