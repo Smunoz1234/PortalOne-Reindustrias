500d5dfb422fdfd427d5aedf647f3192ef9595cd
Stiven Muñoz Murillo <smunoz@nedugatech.com>
2024-03-06 15:17:18 -0500

    ajustes varios

--- jerarquias_componentes.php
+++ jerarquias_componentes.php
@@ -1,16 +1,4 @@
 <?php
-//$Cons_Menu="Select * From uvw_tbl_Categorias";
-//$SQL_Menu=sqlsrv_query($conexion,$Cons_Menu);
-$SQL_Menu = Seleccionar('uvw_tbl_Categorias', '*');
-$json = "";
-while ($row_Menu = sqlsrv_fetch_array($SQL_Menu)) {
-	if ($row_Menu['ID_Padre'] == 0) {
-		$row_Menu['ID_Padre'] = "#";
-	}
-	$json = $json . "{'id':'" . $row_Menu['ID_Categoria'] . "', 'parent': '" . $row_Menu['ID_Padre'] . "', 'text': '" . $row_Menu['NombreCategoria'] . "', 'state': { 'opened': false}},";
-}
-//echo $json;
-
 // Componentes, Tarjeta de Equipo. SMM, 06/03/2024
 $SQL_Componentes_TE = Seleccionar("uvw_tbl_TarjetaEquipo_Componentes", "*", "id_tarjeta_equipo_padre = $IdTarjetaEquipo");
 
@@ -26,92 +14,64 @@ while ($row_Componente = sqlsrv_fetch_array($SQL_Componentes_TE)) {
 // SMM, 06/03/2024
 $SQL_Padre = Seleccionar("uvw_Sap_tbl_TarjetasEquipos", "*", "IdTarjetaEquipo = $IdTarjetaEquipo");
 $row_Padre = sqlsrv_fetch_array($SQL_Padre);
-
 $textPadre = ($row_Padre["ItemCode"] ?? "") . " - " . ($row_Padre["ItemName"] ?? "");
 ?>
+
 <div class="row">
 	<div class="col-lg-12">
 		<div class="ibox-content">
 			<?php include("includes/spinner.php"); ?>
-			<?php
-			//$Cons_Menu="Select * From uvw_tbl_Categorias Where ID_Padre=0";
-			//$SQL_Menu=sqlsrv_query($conexion,$Cons_Menu,array(),array( "Scrollable" => 'static' ));
-			//$Num_Menu=sqlsrv_num_rows($SQL_Menu);
-			?>
-			<div id="jstree1">
-
-			</div>
-			<div id="event_result"></div>
+			<div id="jstree_components"></div>
 		</div>
 	</div>
-	<!-- InstanceEndEditable -->
-
 </div>
-<!-- InstanceBeginEditable name="EditRegion4" -->
+
 <script>
 	$(document).ready(function () {
-		$(".alkin").on('click', function () {
-			$('.ibox-content').toggleClass('sk-loading');
+		$(".alkin").on("click", function () {
+			$(".ibox-content").toggleClass("sk-loading");
 		});
 
-		$('#jstree1')
-			/*.on('changed.jstree', function (e, data) {
-				var i, j, r = [];
-				for(i = 0, j = data.selected.length; i < j; i++) {
-				  r.push(data.instance.get_node(data.selected[i]).text);
-				}
-				//$('#event_result').html('Selected: ' + r.join(', '));
-			})*/
-			.jstree({
-				'core': {
-					'check_callback': function () {
-						//alert('Prueba');
-					},
-					'strings': {
-						'Loading ...': 'Cargando...'
-					},
-					'multiple': false,
-					'data': [
-						{
-							"text": "<?php echo $textPadre; ?>",
-							"icon": "fa fa-sitemap",
-							"children": [
-								<?php foreach ($array_Componentes as &$component) { ?>
-									{
-										"text": "<?php echo $component["id_articulo_hijo"] . " - " . $component["articulo_hijo"]; ?>",
-										"icon": "fa fa-cubes"
-									
-										<?php /* ?>
-											, "children": [
-												{
-													"text": "Nieto",
-													"icon": "fa fa-cube"
-												}
-											]
-										<?php */ ?>
-									},
-								<?php } ?>
-							]
-						}
-					]
+		$("#jstree_components").jstree({
+			"core": {
+				"strings": {
+					"Loading ...": "Cargando..."
 				},
-				'get_selected': true
-				, "plugins": ["themes", "icons"]
-			})
-			.bind('select_node.jstree', function (e, data) {
-				Editar(data.node.id);
-			});
-
-		console.log([<?php echo $json; ?>]);
+				"multiple": false,
+				"data": [
+					{
+						"text": "<?php echo $textPadre; ?>",
+						"icon": "fa fa-sitemap",
+						"children": [
+							<?php foreach ($array_Componentes as &$component) { ?>
+								{
+									"text": "<?php echo $component["id_articulo_hijo"] . " - " . $component["articulo_hijo"]; ?>",
+									"icon": "fa fa-cubes"
+								
+									<?php /* ?>
+										, "children": [
+											{
+												"text": "Nieto",
+												"icon": "fa fa-cube"
+											}
+										]
+									<?php */ ?>
+								},
+							<?php } ?>
+						]
+					}
+				]
+			},
+			'get_selected': true
+			, "plugins": ["themes", "icons"]
+		})
+		.bind('select_node.jstree', function (e, data) {
+			Seleccionar(data.node.id);
+		});
 	});
-</script>
-<script>
-	function Editar(id) {
-		//alert("Has seleccionado el nodo "+id);
-		//var div_edit=document.getElementById('div_Edit');
-		var a_Edit = document.getElementById('a_Edit');
-		a_Edit.style.visibility = 'visible';
-		a_Edit.setAttribute('href', 'categorias.php?id=' + Base64.encode(id) + '&tl=1');
+
+	// SMM, 06/03/2024
+	function Seleccionar(id) {
+		alert(`Has seleccionado el nodo "${id}"`);
 	}
 </script>
-<!-- InstanceEndEditable -->
