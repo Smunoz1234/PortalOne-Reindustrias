cd34e678d1d722643ccfc2133b0ad1fb5949d63d
Stiven Muñoz Murillo <smunoz@nedugatech.com>
2024-05-10 15:28:52 -0500

    entorno?

---
+++ includes/entorno.php
@@ -0,0 +1,6 @@
+<?php
+$tipo_base_datos = 1; // 1-SQL SERVER & 2-SAP HANA
+$nombre_bd = "PortalOneReindustrias_Pruebas";
+$usuario_bd = "portalone";
+$password_bd = "N3dug4t3ch#2021*";
+$servidor_bd = "172.30.5.10\SQL1";
